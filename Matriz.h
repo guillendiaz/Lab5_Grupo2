cc46f94dcf12eff5a4b5a0e90ee6faca94ab5320
guillendiaz <guillendiazd@unitec.edu>
2017-08-25 13:22:48 -0600

    Inicio

---
+++ Matriz.h
@@ -0,0 +1,18 @@
+#ifndef MATRIZ_H
+#define MATRIZ_H
+
+
+
+class Matriz {
+	private:
+		int Fila;
+		int Columna;
+
+	public:
+		void setFila(int);
+		int getFila();
+		void setColumna(int);
+		int getColumna();
+};
+
+#endif
